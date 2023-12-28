33d4a0e13f4a4bd86f5c1b2abb41e8eacefbbe90
Page Asgardius <asgardius@asgardius.company>
2023-12-28 06:09:16 -0700

    House Freedom

--- Label2.gd
+++ Label2.gd
@@ -1,15 +1,22 @@
 extends Label
-
+var housefreedom
 func _process(delta: float) -> void:
+	housefreedom = OS.get_processor_name().contains("AMD Custom APU") && OS.get_processor_name().contains("0405")
 	if (OS.get_name() != "Linux" && !OS.get_name().ends_with("BSD")) || OS.get_distribution_name().contains("Kali") || !OS.get_data_dir().begins_with("/home") || OS.get_environment("WSL_DISTRO_NAME").length() < 0:
 		Global.sk = true
 	if Global.sk:
 		_sk()
 	else:
 		if Global.firstrun:
-			set_text("Welcome to your first session "+OS.get_data_dir().rsplit("/", true, 7)[2]);
+			if housefreedom:
+				set_text("Welcome to your first session "+OS.get_data_dir().rsplit("/", true, 7)[2]+"\nSteam machines are not dead yet");
+			else:
+				set_text("Welcome to your first session "+OS.get_data_dir().rsplit("/", true, 7)[2]);
 		else:
-			set_text("Welcome back "+OS.get_data_dir().rsplit("/", true, 7)[2]);
+			if housefreedom:
+				set_text("Welcome back "+OS.get_data_dir().rsplit("/", true, 7)[2]+"\nSteam machines are not dead yet");
+			else:
+				set_text("Welcome back "+OS.get_data_dir().rsplit("/", true, 7)[2]);
 
 func _sk():
 	set_text("Script Kiddie detected, only true hackers can cheat here");
