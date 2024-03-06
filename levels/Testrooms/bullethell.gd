e2b20fc9459c8ee0b5800298ae09860edef2f709
Page Asgardius <asgardius@asgardius.company>
2024-03-06 12:48:56 -0700

    perspective

--- levels/Testrooms/bullethell.gd
+++ levels/Testrooms/bullethell.gd
@@ -1,9 +1,11 @@
 extends Node2D
 var talk = load("res://levels/ui/talk.tscn").instantiate()
 var boss
+var player
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	boss = $Natasha
+	player = $Player
 	boss.add_to_group("boss")
 	Global.cdialog = [["What are you doing here?", true, 0, 0], ["Your father need help to debug this game", false, 9], ["Ok, i'll help too", true, 0, 0]]
 	Global.live = 0
@@ -12,8 +14,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if player.position.y < boss.position.y && player.get_index() > boss.get_index():
+		move_child(player, boss.get_index() - 1)
+		print("move up")
+	elif player.position.y > boss.position.y && player.get_index() < boss.get_index():
+		move_child(player, boss.get_index() + 1)
+		print("move down")
+	#pass
 
 func _input(event):
 	if Global.cdialog.size() == 0:
 		get_tree().root.remove_child(talk)
+	
--- levels/Testrooms/lilytest.gd
+++ levels/Testrooms/lilytest.gd
@@ -1,12 +1,20 @@
 extends Node2D
 var boss
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	boss = $Lily
+	player = $Player
 	boss.add_to_group("boss")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if player.position.y < boss.position.y && player.get_index() > boss.get_index():
+		move_child(player, boss.get_index() - 1)
+		print("move up")
+	elif player.position.y > boss.position.y && player.get_index() < boss.get_index():
+		move_child(player, boss.get_index() + 1)
+		print("move down")
+	#pass
