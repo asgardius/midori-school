396fdfe1b961195fddf723b47b36c1c5728ec072
Page Asgardius <asgardius@asgardius.company>
2024-03-12 18:32:04 -0700

    new release

---
+++ levels/ui/vmboot.gd
@@ -0,0 +1,23 @@
+extends Node2D
+const backgrounds = ["res://backgounds/booting.png", "res://backgounds/fsck.png"]
+var stage = 0
+var bg
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	bg = $TextureRect
+	bg.texture = load(backgrounds[stage])
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	if stage == 0:
+		stage = stage + 1
+		bg.texture = load(backgrounds[stage])
+	else:
+		get_tree().change_scene_to_file("res://backgounds/disclaimer.tscn")
+	#pass # Replace with function body.
