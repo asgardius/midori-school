6440bf4737b6435df98fce21b29b6db7d0d4479b
Page Asgardius <asgardius@asgardius.company>
2024-11-11 10:39:03 -0700

    ltc

--- levels/Ceres/honkerrailway.gd
+++ levels/Ceres/honkerrailway.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var talk
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if !Global.debug:
@@ -15,8 +15,36 @@ func _ready():
 func _process(delta):
 	pass
 
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept") && Global.cdialog != []:
+		if Global.live == 1:
+			Global.live = 0
+			talk = load("res://levels/ui/talk.tscn").instance()
+			call_deferred("_talk")
+
 func _statrebase():
 	for i in 4:
 		if Global.party[i][0] != null:
 			for j in 7:
 				Global.cstats[Global.party[i][0]][j] = Global.basestats[Global.party[i][0]][j] * Global.level[Global.dparty[i][0]]
+
+
+func _on_Ltctalk_body_entered(body):
+	if body.is_in_group("players"):
+		Global.cdialog = [[tr("DIALOG_HKR_0"), true, 0, 0]]
+
+
+func _on_Ltctalk_body_exited(body):
+	Global.cdialog = []
+
+
+func _on_Rgtalk_body_entered(body):
+	if body.is_in_group("players"):
+		Global.cdialog = [[tr("DIALOG_HKR_1"), false, 1]]
+
+
+func _on_Rgtalk_body_exited(body):
+	Global.cdialog = []
+
+func _talk():
+	get_tree().root.add_child(talk)
--- levels/Earth/map.gd
+++ levels/Earth/map.gd
@@ -1,11 +1,58 @@
 extends Node2D
+var hkr
+var troublemaker
+var travel = null
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hkr = $Railwaygirl
+	troublemaker = $Pamela
+	if !Global.debug:
+		_statrebase()
+	Global.live = 1
+	if Global.quest[1] == 0:
+		hkr.queue_free()
+	if Global.quest[2] == 0:
+		troublemaker.queue_free()
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept") && travel != null:
+		if Global.ccutscene == null:
+			if Global.debug:
+				Global.dplace = travel
+			else:
+				Global.cplace = travel
+			Global.live = 5
+		else:
+			Global.live = 8
+
+func _statrebase():
+	for i in 4:
+		if Global.party[i][0] != null:
+			for j in 7:
+				Global.cstats[Global.party[i][0]][j] = Global.basestats[Global.party[i][0]][j] * Global.level[Global.dparty[i][0]]
+
+
+func _on_railwaygirl_body_entered(body: Node2D) -> void:
+	Global.ccutscene = 10
+	travel = [1, 8, 11]
+
+
+func _on_railwaygirl_body_exited(body: Node2D) -> void:
+	travel = null
+
+
+func _on_pamela_body_entered(body: Node2D) -> void:
+	Global.ccutscene = 13
+	travel = [1, 1, 17]
+
+
+func _on_pamela_body_exited(body: Node2D) -> void:
+	travel = null
