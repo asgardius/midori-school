17cdce4c45dc4f8cc07f6661af35db3ab6821918
Page Asgardius <asgardius@asgardius.company>
2024-12-16 16:31:16 -0700

    new alsa test

--- levels/ui/alsatest.gd
+++ levels/ui/alsatest.gd
@@ -15,31 +15,35 @@ func _ready() -> void:
 	add_child(bgsound)
 	#add_child(fl)
 	#add_child(fr)
-	bgsound.stream = load(Global.musictracks[21])
+	bgsound.stream = load("res://music/c/alsa.ogg")
 	bgsound.mix_target = 1
 	bgsound.bus = "Music"
 	#fl.mix_target = 1
 	#fr.mix_target = 1
 	#fl.stream = load("res://sfx/Front_Left.wav")
 	#fr.stream = load("res://sfx/Front_Right.wav")
-	bgsound.set_volume_db(-10)
+	#bgsound.set_volume_db(-10)
 	#fl.set_volume_db(10)
 	#fr.set_volume_db(10)
 	bgsound.play(0)
+	fl.play(0)
+	fr.play(0)
+	rl.play(0)
+	rr.play(0)
 
 func _input(event):
 	gamepadtest.new(event)
    # Mouse in viewport coordinates.
 	if Input.is_action_just_pressed("ui_cancel") || Input.is_action_just_pressed("mclick"):
 		get_tree().change_scene("res://levels/ui/soundtest.tscn")
-	elif Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_up"):
-		fl.play(0)
-	elif Input.is_action_pressed("ui_right") && Input.is_action_pressed("ui_up"):
-		fr.play(0)
-	elif Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_down"):
-		rl.play(0)
-	elif Input.is_action_pressed("ui_right") && Input.is_action_pressed("ui_down"):
-		rr.play(0)
+	#elif Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_up"):
+	#	fl.play(0)
+	#elif Input.is_action_pressed("ui_right") && Input.is_action_pressed("ui_up"):
+	#	fr.play(0)
+	#elif Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_down"):
+	#	rl.play(0)
+	#elif Input.is_action_pressed("ui_right") && Input.is_action_pressed("ui_down"):
+	##	rr.play(0)
 	#if event is InputEventMouseMotion:
 	#	bgsound.position = Vector3(event.position.x-640, 0, event.position.y-480)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
