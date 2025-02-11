80df59ae979dfb5c22ccb0a1f46134e36e551213
Page Asgardius <asgardius@asgardius.company>
2025-02-10 17:17:19 -0700

    chicken roll

--- levels/Cutscenes/smugface/Alice.gd
+++ levels/Cutscenes/smugface/Alice.gd
@@ -1,12 +1,17 @@
 extends Control
+var croll = load("res://levels/challenges/croll.tscn").instance()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if !Global.debug:
 		Global.cplace = [0, 0, 0]
+		call_deferred("_croll")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _croll():
+	get_tree().root.add_child(croll)
