342c31e377ae4a38c7346186793fd8ebed90b70e
Page Asgardius <asgardius@asgardius.company>
2026-09-15 04:35:19 -0700

    no disc no buy

--- gamepad.gd
+++ gamepad.gd
@@ -5,7 +5,7 @@ func _init(event):
 		Global.gamepad = 0
 	elif str(Input.get_joy_name(0)).begins_with("Nintendo Switch"):
 		Global.gamepad = 2
-	elif str(Input.get_joy_name(0)).begins_with("Sony Computer Entertainment") || str(Input.get_joy_name(0)).begins_with("PS4") || str(Input.get_joy_name(0)).begins_with("PS5"):
+	elif str(Input.get_joy_name(0)).begins_with("Sony") || str(Input.get_joy_name(0)).begins_with("PS4") || str(Input.get_joy_name(0)).begins_with("PS5"):
 		Global.gamepad = 3
 	elif str(Input.get_joy_name(0)).length() != 0:
 		Global.gamepad = 1
